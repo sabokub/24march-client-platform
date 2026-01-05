9df1399d5ae649729a211e7b0bb49b62a0fef90a
sabokub <sab_iina@hotmail.fr>
2026-01-05 02:55:43 +0100

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,8 @@ cat > components/ui/input.tsx <<'EOF'
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => (
